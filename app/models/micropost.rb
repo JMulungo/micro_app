b8429ef1f4b37c539616e02d256eb87c2ed30953
Jorge Mulungo <jj.mulungo@gmail.com>
2013-07-19 18:12:30 +0200

    modified micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,6 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :resources, :user_id
+  belongs_to :user
+  validates :resources, :length => { maximum: 140}
+  #validates_length_of :resources, :within => 3..20 , :message => " - minimo de 3 e maximo de 20 caracteres"
 end
